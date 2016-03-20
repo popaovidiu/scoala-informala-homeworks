08126977640cd26c5cb43e94d09e2fa465f27fec
Popa Ovidiu <popaovidiu2004@gmail.com>
2016-03-20 16:32:22 +0200

    Revert "Tema 3 - varianta finala"
    This reverts commit cbbb61b68ac64c0f63421bcbbe549ae864e8f290.

--- java-2016/week-3/Library.java
+++ java-2016/week-3/Library.java
@@ -1,102 +1,28 @@
-// A test driver for Library
-
-import java.util.ArrayList;
-import java.util.Scanner;
+// A test driver for the Novels and albums classes
 
 public class Library {
    
-   
-    
-	ArrayList<String> library_books = new ArrayList<String>(); 
-	ArrayList<String> library_albums = new ArrayList<String>(); 
-	Novel dummy_novel = new Novel("","","");
-	Album dummy_album = new Album("","","");
-	Scanner input = new Scanner(System.in);
-	
-	
-	//Library is populated with objects: novels and albums
-	
-	public void addNovel(Novel novel){	
-		
-		System.out.println("How many novels are there in the library?   ");
-		int novelNumbers = input.nextInt();
-	
-		for (int i=0; i<novelNumbers; i++){
-			String title = dummy_novel.getTitle();
-			String type = dummy_novel.getType();
-			String pageNumber = dummy_novel.getPageNumber();
-			library_books.add(title);
-			library_books.add(type);
-			library_books.add(pageNumber);
-			}
-		System.out.println("------------------------------------------------------------");//For me to monitor the console more easily
-	}
-	
-	public void addAlbum(Album album){ 		
-		System.out.println("How many art albums are there in the library?   ");
-		int albumNumbers = input.nextInt();
-	
-		for (int i=0; i<albumNumbers; i++){
-			String title = dummy_album.getTitle();
-			String quality = dummy_album.getQuality();
-			String pageNumber = dummy_album.getPageNumber();
-			library_albums.add(title);
-			library_albums.add(quality);
-			library_albums.add(pageNumber);
-			}
-		System.out.println("------------------------------------------------------------");//For me to monitor the console more easily
-	}
-	//The content of the library is listed
-	
-	public void listContent(){
-		int bookNumber = 1;
-		System.out.println("The novels are:");
-		for (int i=0; i< library_books.size();i=i+3){
-			System.out.println("Catalog number "+bookNumber+": " + library_books.get(i) +" "+library_books.get(i+1)+" "+library_books.get(i+2));
-			bookNumber++;
-			}
-		System.out.println("------------------------------------------------------------");//For me to monitor the console more easily
-		
-		int albumNumber = 1;
-		System.out.println("The art albums are:");
-		for (int i=0; i< library_albums.size();i=i+3){
-			System.out.println("Catalog number "+albumNumber+": " + library_albums.get(i) +" "+library_albums.get(i+1)+" "+library_albums.get(i+2));
-			albumNumber++;
-			}
-		System.out.println("------------------------------------------------------------");//For me to monitor the console more easily
-	}
-	
-	
-	//Some of the content is removed
-	
-	
-	public void deleteNovel(Novel novel){
-		System.out.println("Do you want to delete a novel from the library? 1=yes/0=no"); 
-		int deleteChoiceNovel = input.nextInt();
-	
-		if (deleteChoiceNovel == 1){
-			System.out.println("What novel do you want to delete (type the catalog number): ");
-			int novelCatalogNumber = input.nextInt();
-			library_books.remove(3*novelCatalogNumber-3);
-			library_books.remove(3*novelCatalogNumber-3);
-			library_books.remove(3*novelCatalogNumber-3);
-		}
-	}
-
-	public void deleteAlbum(Album album){
-		System.out.println("Do you want to delete an art album from the library? 1=yes/0=no"); 
-		int deleteChoiceAlbum = input.nextInt();	
-		if (deleteChoiceAlbum == 1){	
-			System.out.println("What art album do you want to delete (type the catalog number): ");
-			int albumCatalogNumber = input.nextInt();
-			library_albums.remove(3*albumCatalogNumber-3);
-			library_albums.remove(3*albumCatalogNumber-3);
-			library_albums.remove(3*albumCatalogNumber-3);
-			 
-		} 
-	}	
-		
+   public static void main(String[] args) {
+      
+	  Novels dummy_novel = new Novels("LOTR", "fantasy", 152);
+      System.out.println(dummy_novel);  
+	  
+	  
+	  dummy_novel.getTitle();
+	  dummy_novel.getType();
+	  dummy_novel.getPageNumber();
+	  System.out.println(dummy_novel); 
+	  
 	 
-	 
-
+	  
+	  Albums dummy_album = new Albums("Monet", "gold", 352);
+      System.out.println(dummy_album);  
+	  
+	  dummy_album.getTitle();
+	  dummy_album.getQuality();
+	  dummy_album.getPageNumber();
+	  System.out.println(dummy_album);
+	  
+	  
+   }
 }
